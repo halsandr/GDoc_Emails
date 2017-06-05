ab04e25b6d15034b89c43f6e294138c300df3e03
Andy <halsandr@hotmail.co.uk>
2017-06-05 15:15:56 +0100

    Cleaning up local examples

--- client_list_example.py
+++ client_list_example.py
@@ -1,6 +1,6 @@
 ourClients = {
-            'shinfield': '1fUgl1JnrAN8s7ZWrGAhqw6yL0jGvCZ7zLxM8ZAlGGJ8',
-            'ryefield': '14Bz5oSjH-vIPFinEuespzZy8mokd8PkyWcTGcs7Pb7Y',
-            'cumnor': '1ydJqgrdeFvjPFvz4AkEf4_H59XRLS6fz5Rg6aWy2zos',
-            'leycester': '1-j-fWNSwaXEeVa2XZCrfMxMnotIKQytV4YaKDjSj-Q0'
+            'client1': '1ydJqghdfhdfhDFHDvz435GGDFGEf4_H59XRLSe558rt',
+            'client2': 'jfhhJDfhhfi48588_jashsdfskiFJS245345sfgDDDDe',
+            'client3': '4JGbbowu678_fkkkGGGeooajdj45672SDFSDG35gskkk',
+            'client4': 'gjjjjjGJGJGJ559999_akkkddjfshhSKDKDK4554663F'
         }
