d6e89cbc43e17e82ad08949decf21021a311b195
Andy <halsandr@hotmail.co.uk>
2017-06-05 13:08:03 +0100

    Create example client_list.py
    This file only contains made up data

---
+++ client_list.py
@@ -0,0 +1,6 @@
+ourClients = {
+            'client1': '1ydJqghdfhdfhDFHDvz435GGDFGEf4_H59XRLSe558rt',
+            'client2': 'jfhhJDfhhfi48588_jashsdfskiFJS245345sfgDDDDe',
+            'client3': '4JGbbowu678_fkkkGGGeooajdj45672SDFSDG35gskkk',
+            'client4': 'gjjjjjGJGJGJ559999_akkkddjfshhSKDKDK4554663F'
+        }
